1c35652911d8c4e86d45059475e5a75423b6d23c
braga493 <22401725@aluno.cotemig.com.br>
2026-05-26 10:39:36 -0300

    Add files via upload

---
+++ jinja_aula2/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def questao1():
+    return render_template("questao1.html",name ="Lucas")
+
+@app.route("/questao2")
+def questao2():
+    dados = [{"nome":"vargas","idade":22}]
+    return render_template("questao2.html",alunos = dados)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
